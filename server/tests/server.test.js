e8e1d33c427313e2f013ca768e9cdbda26765016
Yiwen Gong <gongyiwen2017@gmail.com>
2017-07-14 18:12:18 -0400

    Addd test for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -140,3 +140,42 @@ describe('DELETE /todos/:id', ()=> {
   });
 
 });
+
+describe('PATCH /todos/:id', ()=> {
+  it('should update a todo', (done) =>{
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) =>{
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the new text!!';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+
+});
