4e19d81ffeb37af9560cac2dc18f935f919d217d
Yiwen Gong <gongyiwen2017@gmail.com>
2017-07-16 13:22:38 -0400

    add Post /users and token method

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
 
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -89,6 +90,20 @@ app.patch('/todos/:id', (req, res) => {
   });
 
 })
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 
 
 
